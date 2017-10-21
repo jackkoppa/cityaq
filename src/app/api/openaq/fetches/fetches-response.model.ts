fe7648b160b42ebb494f732537aa2b5272af361d
RobertImbrie <robert.m.imbrie@gmail.com>
2017-10-21 13:19:06 -0500

    Fixing tyes for request/response models

--- src/app/api/openaq/fetches/fetches-response.model.ts
+++ src/app/api/openaq/fetches/fetches-response.model.ts
@@ -1,8 +1,8 @@
 // https://docs.openaq.org/#api-Fetches
 
 export class FetchesResponseModel {
-    timeStarted: datetime;
-    timeEnded: datetime;
+    timeStarted: string;
+    timeEnded: string;
     count: number;
     results: object;
 }
