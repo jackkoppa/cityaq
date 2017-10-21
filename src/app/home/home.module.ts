88eeb153370de28275a8433ff55fae389cc1de21
Jack Koppa <hello@jackkoppa.com>
2017-10-21 11:54:10 -0500

    Add Angular Material; test Material components

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatButtonModule } from '@angular/material';
 import { HomeComponent } from './home.component';
 
 @NgModule({
     declarations: [HomeComponent],
+    imports: [
+        BrowserAnimationsModule,
+        MatCardModule,
+        MatButtonModule
+    ],
     exports: [HomeComponent]
 })
 export class HomeModule {}
