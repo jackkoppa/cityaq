1061f9a0710086ab935f7a5fa552aededd747674
Jack Koppa <hello@jackkoppa.com>
2017-10-21 11:34:46 -0500

    Import HomeModule into App; add working Home template

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'viz-home',
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {
+    testMsg: string = "Hello - I am Home."
+}
