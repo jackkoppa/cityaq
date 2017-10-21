913b83cacbd3886a536fd42c7d59b829d69a2f77
RobertImbrie <robert.m.imbrie@gmail.com>
2017-10-21 13:32:40 -0500

    Fixing types for fetchesresponsemodel

--- src/app/api/openaq/fetches/fetches-response.model.ts
+++ src/app/api/openaq/fetches/fetches-response.model.ts
@@ -1,13 +1,8 @@
 // https://docs.openaq.org/#api-Fetches
 
 export class FetchesResponseModel {
-<<<<<<< HEAD
     timeStarted: string;
     timeEnded: string;
-=======
-    timeStarted: Date;
-    timeEnded: Date;
->>>>>>> cfbd351b09e0ce395292614fb07637ad2a825c04
     count: number;
     results: object;
 }
