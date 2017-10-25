990635339348642cb55cd5daea5a177a2975cf36
Jack Koppa <hello@jackkoppa.com>
2017-10-25 11:26:50 -0500

    Add method structures to SearchDisplayService

--- src/app/search-display/search-display.service.spec.ts
+++ src/app/search-display/search-display.service.spec.ts
@@ -1,5 +1,22 @@
 import { SearchDisplayService } from './search-display.service';
 
 describe('SearchDisplayService', () => {
+    let searchDisplayService: SearchDisplayService;
+    beforeEach(() => searchDisplayService = new SearchDisplayService());
 
+    describe('filterCities', () => {
+
+    });
+
+    describe('sortCities', () => {
+
+    });
+
+    describe('pm25Color', () => {
+
+    });
+
+    describe('validateLatestResponse', () => {
+
+    });
 });
