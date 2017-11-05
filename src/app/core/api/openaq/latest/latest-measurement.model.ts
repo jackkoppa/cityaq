ac70e183d4942e07e51c9548e518f34623552f97
Jack Koppa <hello@jackkoppa.com>
2017-11-04 23:17:03 -0500

    Fix date type (change to string)

--- src/app/core/api/openaq/latest/latest-measurement.model.ts
+++ src/app/core/api/openaq/latest/latest-measurement.model.ts
@@ -3,7 +3,7 @@ import { AveragingPeriod } from './averaging-period.model';
 export interface LatestMeasurement {
     parameter: string;
     value: number;
-    lastUpdated: Date;
+    lastUpdated: string;
     unit: string;
     sourceName: string;
     averagingPeriod: AveragingPeriod
