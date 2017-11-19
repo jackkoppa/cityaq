98ec056330026b6bc4dde776947d1d44623baac6
Jack Koppa <hello@jackkoppa.com>
2017-11-18 22:19:12 -0600

    Fix interface extension with new generic

--- src/app/core/calculation/indices/level.model.ts
+++ src/app/core/calculation/indices/level.model.ts
@@ -1,12 +1,4 @@
 import { LevelBase } from './level-base.model';
 import { Range } from './range.model';
 
-export interface Level extends LevelBase {
-    good: Range;
-    moderate: Range;
-    unhealthySensitive: Range;
-    unhealthy: Range;
-    veryUnhealthy: Range;
-    hazardous1: Range;
-    hazardous2: Range;
-}  
+export interface Level extends LevelBase<Range> { }  
