5864c9e512e931ce51bf06cc48ec18b22a812cf5
Jack Koppa <hello@jackkoppa.com>
2017-11-23 09:11:19 -0600

    Simple tests for IntroComponenet

--- src/app/home/intro.component.spec.ts
+++ src/app/home/intro.component.spec.ts
@@ -1,5 +1,36 @@
 import { IntroComponent } from './intro.component';
+import { DebugElement, NO_ERRORS_SCHEMA }    from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
 
 describe('IntroComponent', () => {
+    let fixture: ComponentFixture<IntroComponent>;
+    let introComponent: IntroComponent;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [IntroComponent],
+            schemas: [NO_ERRORS_SCHEMA]
+        });
+        fixture = TestBed.createComponent(IntroComponent);
+        introComponent = fixture.componentInstance;
+        fixture.detectChanges(); 
+    });
+    
+    describe('constructor', () => {
+        it('should initialize the component', () => {
+            expect(introComponent).toBeTruthy();
+        });
 
+        it('should populate the h1 tag correctly', () => {
+            let h1Debug: DebugElement = fixture.debugElement.query(By.css('h1'));;
+            let h1: HTMLElement = h1Debug.nativeElement;   
+            expect(h1.textContent).toContain('Welcome to cityAQ');
+        });
+
+        it('should populate the h3 tag correctly', () => {
+            let h3Debug: DebugElement = fixture.debugElement.query(By.css('h3'));;
+            let h3: HTMLElement = h3Debug.nativeElement;   
+            expect(h3.textContent).toContain('Compare air quality for cities by searching below');
+        });
+    });
 });
