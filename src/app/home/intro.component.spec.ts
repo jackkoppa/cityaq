e689778c493a648970fde36ef149f4bfeb0ee87e
Jack Koppa <hello@jackkoppa.com>
2017-11-23 09:17:57 -0600

    Add image test to IntroComponent spec

--- src/app/home/intro.component.spec.ts
+++ src/app/home/intro.component.spec.ts
@@ -22,15 +22,21 @@ describe('IntroComponent', () => {
         });
 
         it('should populate the h1 tag correctly', () => {
-            let h1Debug: DebugElement = fixture.debugElement.query(By.css('h1'));;
+            let h1Debug: DebugElement = fixture.debugElement.query(By.css('h1'));
             let h1: HTMLElement = h1Debug.nativeElement;   
             expect(h1.textContent).toContain('Welcome to cityAQ');
         });
 
         it('should populate the h3 tag correctly', () => {
-            let h3Debug: DebugElement = fixture.debugElement.query(By.css('h3'));;
+            let h3Debug: DebugElement = fixture.debugElement.query(By.css('h3'));
             let h3: HTMLElement = h3Debug.nativeElement;   
             expect(h3.textContent).toContain('Compare air quality for cities by searching below');
         });
+
+        it('should have the correct source for the svg image', () => {
+            let imgDebug: DebugElement = fixture.debugElement.query(By.css('img'));
+            let img: HTMLImageElement = imgDebug.nativeElement;
+            expect(img.src).toBe('http://localhost:9876/assets/icons/cityAQ.svg');
+        })
     });
 });
