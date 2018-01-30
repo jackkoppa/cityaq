b1b99554003d65b65bd5059c2311be71c93bdc16
Jack Koppa <hello@jackkoppa.com>
2018-01-30 07:17:53 -0600

    Write initial text for second onboarding slide

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { FadeAnimation } from '../shared/animations/fade-animation.constant';
 import { SearchService } from '../search/search.service';
 import { SearchingStatus } from '../search/searching-status.model';
+import { QueryParams } from '../core/routing/params.models';
 
 @Component({
     selector: 'aq-home',
@@ -14,8 +15,12 @@ export class HomeComponent {
     userOnboarded: boolean = false;
     
     constructor(
+        private route: ActivatedRoute,
         private searchService: SearchService
     ) { 
+        this.route.queryParams.subscribe(queryParams => {
+            if (queryParams.cities && queryParams.cities.length > 0) this.userOnboarded = true;
+        })
         this.searchService.searchingStatus.subscribe(status => {
             if (status === SearchingStatus.Focused) this.userOnboarded = true;
         });
