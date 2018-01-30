fff763b229fc947b0e593cc4461053eb22682473
Jack Koppa <hello@jackkoppa.com>
2018-01-29 19:33:56 -0600

    Improve slide-delete animation to auto-close on next attempt after locking Button should automatically display when slid past button width And should automatically hide on next slide attempt

--- src/app/shared/components/slide-delete.component.ts
+++ src/app/shared/components/slide-delete.component.ts
@@ -1,6 +1,5 @@
 import { 
     Component,
-    OnInit,
     Output,
     EventEmitter,
     HostListener,
@@ -8,47 +7,69 @@ import {
     ElementRef
 } from '@angular/core';
 
-const VELOCITY_THRESHOLD: number = 0.2;
+const VELOCITY_THRESHOLD: number = 0.15;
 const DELETE_BUTTON_WIDTH: number = 75;
+const DELAY_CORRECTION_PX: number = 20;
 
 @Component({
     selector: 'aq-slide-delete',
     templateUrl: './slide-delete.component.html'
 })
-export class SlideDeleteComponent implements OnInit {
+export class SlideDeleteComponent {
     public x: number = 0;
+    private startX: number = 0;
 
     constructor(
         private renderer: Renderer2,
         private el: ElementRef
     ) { };
 
-    public ngOnInit(): void {
-        
+    public onPanStart(event: any): void {
+        event.preventDefault();
+        this.startX = this.x;
     }
 
-    onPan(event: any): void {
+    public onPan(event: any): void {
         event.preventDefault();
-        console.log(event.deltaX, event.velocityX, event.angle, event.direction)
-        if (Math.abs(event.velocityX) >= VELOCITY_THRESHOLD) {
+        if (this.aboveThreshold(event.velocityX)) {
             this.x = this.calculateNewX(event.deltaX);
             this.setX();
         }
     }
 
-    onPanEnd(event: any): void {
+    public onPanEnd(event: any): void {
         event.preventDefault();
-        this.x = this.x >= DELETE_BUTTON_WIDTH ? DELETE_BUTTON_WIDTH : 0;
+        if (this.newMovementExceedingButtonWidth())
+            this.x = this.x >= 0 ? DELETE_BUTTON_WIDTH : DELETE_BUTTON_WIDTH * -1;
+        else
+            this.x = 0;
         this.setX();
     }
 
-    private calculateNewX(eventX: number): number {
+    private aboveThreshold(velocityX: number): boolean {
+        return Math.abs(velocityX) >= VELOCITY_THRESHOLD
+    }
+
+    private calculateNewX(eventX: number): number {        
         const absX = Math.abs(eventX);
-        const newX = Math.round(Math.log(absX) / Math.log(1.05));
-        return eventX > 0 ? newX : newX * -1;
+        const newAbsX = Math.round(Math.log(absX) / Math.log(1.05));
+        const newX = (eventX >= 0 ? newAbsX : newAbsX * -1) + this.startX;
+        return this.correctPosition(newX);
     }
     
     private setX(): void {
         this.renderer.setStyle(this.el.nativeElement, 'left', `${this.x}px`);
+    }
+
+    private newMovementExceedingButtonWidth(): boolean {
+        return Math.abs(this.x) >= DELETE_BUTTON_WIDTH && this.startX === 0
+    }
+
+    private correctPosition(newX: number): number {
+        if (this.startX === 0) return newX;
+        const correction = this.startX >= 0 ? DELAY_CORRECTION_PX : DELAY_CORRECTION_PX * -1;
+        const correctedX = newX - correction;
+        const outOfBounds = this.startX >= 0 ? correctedX < 0 : correctedX > 0;
+        return outOfBounds ? 0 : correctedX;
     }
 }
