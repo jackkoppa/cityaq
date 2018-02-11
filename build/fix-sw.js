0b970ac71baa5c8677ee84e4c75e08d84d6076de
Jack Koppa <hello@jackkoppa.com>
2018-02-11 08:41:47 -0600

    Change post-build script to be usable outside this project Make baseHref a variable

--- build/fix-sw.js
+++ build/fix-sw.js
@@ -2,22 +2,29 @@
 *
 * Temporary fix for https://github.com/angular/angular/issues/21636
 *
+* Explained in this Stack Overflow answer: https://stackoverflow.com/questions/48565629/how-to-handle-routing-in-angular-5-service-workers
+*
+* Warning: removes any instances of setting EXISTING_CLIENTS_ONLY state, which will likely have adverse effects in some situations
+*
 */
 
 const replace = require('replace-in-file');
 
-const existing = {
+ // enter the value set as the --base-href flag when deploying to a live URL
+ // if not deploying to a live URL, and only running locally, can leave blank, as the URL parsing fix will not be necessary
+const SITE_BASE_HREF = 'https://jackkoppa.github.io/cityaq/';
+
+const existingStateReplacements = {
     files: 'dist/ngsw-worker.js',
     from: /this\.state = DriverReadyState\.EXISTING_CLIENTS_ONLY;/g,
     to: '/*this.state = DriverReadyState.EXISTING_CLIENTS_ONLY;*/ ' + 
-        '// removing EXISTING_CLIENTS_ONLY state, as it behaves incorrectly in offline GitHub pages testing'
-
+        '// removing EXISTING_CLIENTS_ONLY state, as it behaves incorrectly in offline testing, both locally & on GitHub pages'
 }
 
-const baseHref = {
+const baseHrefInstances = {
     files: 'dist/ngsw.json',
-    from: /\"index\": \"https:\/\/jackkoppa\.github\.io\/cityaq\/index\.html\",/g,
-    to: '"index": "https://jackkoppa.github.io/cityaq/index.html", ',
+    from: '"' + SITE_BASE_HREF + 'index.html",',
+    to: '"' + SITE_BASE_HREF + 'index.html", ', // whitespace-only change indicates that the baseHref was found, so we should make the URL fix
 };
 
 const serviceWorkerURLFix = {
@@ -29,7 +36,7 @@ const serviceWorkerURLFix = {
 }
 
 try {
-    const existingInstances = replace.sync(existing);
+    const existingInstances = replace.sync(existingStateReplacements);
     console.log('Replacements of EXISTING_CLIENTS_ONLY states: ', existingInstances.join(', '));
 }
 catch(error) {
@@ -37,8 +44,8 @@ catch(error) {
 }
 
 try {
-    const baseHrefInstances = replace.sync(baseHref);
-    if (baseHrefInstances && baseHrefInstances.length > 0) {
+    const foundBaseHref = replace.sync(baseHrefInstances);
+    if (foundBaseHref && foundBaseHref.length > 0) {
         try {
             const override = replace.sync(serviceWorkerURLFix);
             console.log('Changes made: ', override.join(', '))
@@ -48,7 +55,7 @@ try {
         }
     }
     else {
-        console.log('baseHref was not set; no URL matching changes needed', baseHrefInstances);
+        console.log('baseHref was not set; no URL matching changes needed', foundBaseHref);
     }
 }
 catch (error) {
