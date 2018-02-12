6928bf2cb4672d05fcaf07349d187f6c9b108958
Jack Koppa <hello@jackkoppa.com>
2018-02-12 07:25:21 -0600

    Import AQI levels for use in explanation slide

--- src/app/onboarding/onboarding-slide-three.component.ts
+++ src/app/onboarding/onboarding-slide-three.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { AQI_LEVELS }  from '../core/calculation/indices/aqi-levels.constant';
+
 @Component({
     selector: 'aq-onboarding-slide-three',
     templateUrl: './onboarding-slide-three.component.html'
